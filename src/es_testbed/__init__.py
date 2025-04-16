c3887f2d6f9ff46e192002b22cd577da5e873412
untergeek <aaron@mildensteins.com>
2025-04-16 10:39:31 -0600

    Make debug easily exportable

--- src/es_testbed/__init__.py
+++ src/es_testbed/__init__.py
@@ -4,5 +4,6 @@ __version__ = '0.11.0'
 
 from es_testbed._base import TestBed
 from es_testbed._plan import PlanBuilder
+from es_testbed.debug import debug
 
-__all__ = ['TestBed', 'PlanBuilder']
+__all__ = ['TestBed', 'PlanBuilder', 'debug']
